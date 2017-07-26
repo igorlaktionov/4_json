cd813318c5a3a1f402413e8db5d540a2675b0396
Igor Laktionov <ilaktionov@iponweb.net>
2017-07-26 18:25:30 +0300

    create pretty_print_json

--- README.md
+++ README.md
@@ -1,17 +1,35 @@
 # Prettify JSON
 
-[TODO. There will be project description]
+Reformats the json data and prints in a pretty format
 
 # Quickstart
-
-[TODO]
 
 Example of script launch on Linux, Python 3.5:
 
 ```#!bash
 
 $ python pprint_json.py <path to file>
-# TODO add output example
+example output:
+[
+    {
+        "Cells": {
+            "PublicPhone": [
+                {
+                    "PublicPhone": "(495) 777-51-95"
+                }
+            ],
+            "Name": "Ароматный Мир",
+            "District": "район Кунцево",
+            "OperatingCompany": "Ароматный Мир",
+            "WorkingHours": [
+                {
+                    "Hours": "09:30-22:30",
+                    "DayOfWeek": "понедельник"
+                }
+            ]
+        }
+    }
+]
 
 ```
 
--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,32 @@
 import json
+import os.path
+import sys
 
 
-def load_data(filepath):
-    pass
+def load_data(file_path):
+    with open(file_path, 'r') as handler:
+        return json.load(handler)
 
 
-def pretty_print_json(data):
-    pass
+def pretty_print_json(json_data):
+    print(json.dumps(json_data, ensure_ascii=False, indent=4))
+
+
+def validate_file_path():
+    if len(sys.argv) < 2:
+        print("Please write argument file path")
+        return None
+    elif not os.path.isfile(sys.argv[1]):
+        print("File not exist")
+        return None
+    elif not os.access(sys.argv[1], os.R_OK):
+        print("File is not readable")
+        return None
+    else:
+        return 1
 
 
 if __name__ == '__main__':
-    pass
+    if validate_file_path():
+        json_data = load_data(sys.argv[1])
+        pretty_print_json(json_data)
